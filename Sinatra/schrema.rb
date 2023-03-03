c58dd8af7cc37ce30b31b8188d158d365476940e
sheema <sheemashamilfarook@gmail.com>
2023-03-03 12:15:58 +0300

    done

--- Sinatra/schrema.rb
+++ Sinatra/schrema.rb
@@ -1,4 +1,12 @@
-class CreateTodos < ActiveRecord :: Migration[6.1]
-    def change
-        create_table :todos do |t|
-            t.string:
+ActiveRecord::Schema.define(version: 2022_01_01_141515) do
+    create_table "categories", force: :cascade do |t|
+      t.string "category" 
+ end
+ 
+ create_table "todos", force: :cascade do |t|
+     t.string "item"
+     t.string "importance"
+     t.string "category_id"
+   end
+ 
+ end
