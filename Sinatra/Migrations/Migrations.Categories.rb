e6884937b60b85aa45d2967f8435b1c925d5b360
sheema <sheemashamilfarook@gmail.com>
2023-03-04 12:07:42 +0300

    completed

--- Sinatra/Migrations/Migrations.Categories.rb
+++
@@ -1,7 +0,0 @@
-class  CreateCategories < ActiveRecord::Migration[6.1]
-    def change
-        create_table :categories do |t|
-            t.string :category_id
-        end
-    end
-end
--- Sinatra/Migrations/migrations.Todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord:: Migration[6.1]
-    def change 
-        create_table :todos do |t|
-            t.string :item
-            t.string :importance
-            t.integer :category_id
-        end
-    end
-end
