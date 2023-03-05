561f635050d30e4ae724033b0044e305471fd711
sheema <sheemashamilfarook@gmail.com>
2023-03-06 00:08:41 +0300

    changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ function Header() {
         <header className="header">
             <img src={trollFace} className="header-image" alt="troll face" />
             <h2 className="header-title">Meme Generator</h2>
+            <button id="login"><a href="login-page-sable-nu.vercel.app">
+Login
+    </a></button>
         </header>
     );
 }
